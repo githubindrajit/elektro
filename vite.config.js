7b7146e0a56f7bd830d82eadab991129f381059b
Indrajit <indrajit.paul@codeclouds.in>
2025-08-01 11:55:50 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/elektro1/'
+  plugins: [react()]
+  // base: '/elektro1/'
 })
 
 
